fb31b19c9fb0511258262ba9e436bdc5e8fa3d87
Basix <jsrtyu1120@gmail.com>
2016-04-09 01:40:19 +0900

    재작성

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
 var twitter = require('twitter');
-var evet = new process.EventEmitter();
+const util = require('util');
+const EventEmitter = require('events');
 
 var bot = undefined
-
 var TwitterBot = function (KEY) {
   bot = new twitter(KEY)
-  this.createTweet = (text, timeout, callback) => {
-    var id = setTimeout(() => {
-      client.post('statuses/update', {status: text},  function(error, tweet, response){
-        if(error) callback(error)
-      });
-    }, timeout);
-    callback(undefined, id)
-  }
-
-  this.removeTweet = (id) => { clearTimeout(id) }
-
-  this.recive = (text) => {
-    client.stream('statuses/filter', {track: text}, function(stream) {
-      stream.on('data', function(tweet) {
-        evet.emit('tweet', tweet)
-      });
-
-      stream.on('error', function(error) {
-        evet.emit('error', error)
-      });
+  EventEmitter.call(this);
+}
+util.inherits(TwitterBot, EventEmitter);
+TwitterBot.prototype.createTweet = (text, timeout, callback) => {
+  var id = setTimeout(() => {
+    client.post('statuses/update', {status: text},  function(error, tweet, response){
+      if(error) callback(error)
     });
-  }
-  
-  this.prototype = evet
+  }, timeout);
+  callback(undefined, id)
+}
+TwitterBot.prototype.removeTweet = (id) => {
+  clearTimeout(id)
+}
+TwitterBot.prototype.recive = (text) => {
+  client.stream('statuses/filter', {track: text}, (stream) => {
+    stream.on('data', (tweet) => {
+      this.emit('data', tweet)
+    });
+    stream.on('error', (error) => {
+      this.emit('error', error)
+    });
+  });
 }
 
-exports = TwitterBot
+module.exports = TwitterBot
