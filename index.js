45ceb82bcf932ef8e1c6383bc3f49fca7a0a938c
Basix <jsrtyu1120@gmail.com>
2016-04-09 01:05:27 +0900

    엄청난 수정

--- index.js
+++ index.js
@@ -5,30 +5,30 @@ var bot = undefined
 
 var TwitterBot = function (KEY) {
   bot = new twitter(KEY)
-}
-TwitterBot.prototype.createTweet = (text, timeout, callback) => {
-  var id = setTimeout(() => {
-    client.post('statuses/update', {status: text},  function(error, tweet, response){
-      if(error) callback(error)
+  this.createTweet = (text, timeout, callback) => {
+    var id = setTimeout(() => {
+      client.post('statuses/update', {status: text},  function(error, tweet, response){
+        if(error) callback(error)
+      });
+    }, timeout);
+    callback(undefined, id)
+  }
+
+  this.removeTweet = (id) => { clearTimeout(id) }
+
+  this.recive = (text) => {
+    client.stream('statuses/filter', {track: text}, function(stream) {
+      stream.on('data', function(tweet) {
+        evet.emit('tweet', tweet)
+      });
+
+      stream.on('error', function(error) {
+        evet.emit('error', error)
+      });
     });
-  }, timeout);
-  callback(undefined, id)
+  }
+  
+  this.prototype = evet
 }
-TwitterBot.prototype.removeTweet = (id) => {
-  clearTimeout(id)
-}
-TwitterBot.prototype.recive = (text) => {
-  client.stream('statuses/filter', {track: text}, function(stream) {
-  stream.on('data', function(tweet) {
-    evet.emit('tweet', tweet)
-  });
-
-  stream.on('error', function(error) {
-    evet.emit('error', error)
-  });
-});
-}
-
-TwitterBot.prototype = evet
 
 exports = TwitterBot
